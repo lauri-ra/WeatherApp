fc0c61ad7c35c80f0c8f3d0e9f2362ca309a827d
Jussi Heinila <heinila.jussi@gmail.com>
2022-12-02 21:58:59 +0200

    Added comments for modelmain.

--- weatherApp/src/main/java/fi/tuni/weatherapp/ModelMain.java
+++ weatherApp/src/main/java/fi/tuni/weatherapp/ModelMain.java
@@ -10,14 +10,30 @@ public class ModelMain {
     
     private HashMap<String,IDataSource> dataSources = new HashMap<>();
     
+    
+    /**
+     * Adds new dataSource to the model
+     * @param dataSource The IDataSource to be added
+     */
     public void AddDataSource(IDataSource dataSource) {
         dataSources.put(dataSource.GetName(), dataSource);
     }
     
+    /**
+     * Function for getting messages between two dates form a given dataSource 
+     * @param dataSourceName Name of the dataSource to be used
+     * @param startDate start date for query
+     * @param endDate end date for query
+     * @return List of message strings
+     */
     public ArrayList<String> GetMessages(String dataSourceName, LocalDate startDate, LocalDate endDate) {
         return dataSources.get(dataSourceName).GetTrafficMessages(startDate, endDate);
     }
     
+    /**
+     * Get the names of al dataSources in the model
+     * @return List of the names of dataSources as Strings
+     */
     public List<String> GetDataSourceNames(){
         ArrayList<String> dataSourceNames = new ArrayList<>();
         
@@ -28,16 +44,36 @@ public class ModelMain {
         return dataSourceNames;
     }
     
+    /**
+     * Get list of variables available from a given dataSource
+     * @param dataSourceName The Name of the dataSource to be queried
+     * @return List of variables
+     */
     public List<Variable> GetVariables(String dataSourceName) {
         
         List<Variable> variables = dataSources.get(dataSourceName).GetVariables();
         return variables;
     }
     
+    /**
+     * Gets a particular variable from a particular dataSource
+     * @param dataSourceName name of the relevant dataSOurce
+     * @param variableName name of the wanted variable
+     * @return variable with the wanted name
+     */
     public Variable GetVariable(String dataSourceName, String variableName) {
         return dataSources.get(dataSourceName).GetVariable(variableName);
     }
     
+    /**
+     * Returns measured data of a variable between two dates in given area
+     * @param dataSourceName Name of the dataSource to be queried
+     * @param variable The variable of which data is queried
+     * @param coordinates the coordinates over to be covered by the data
+     * @param startDate start date of the query
+     * @param endDate end date of the query
+     * @return List of dataPoints representing the queried data
+     */
     public List<DataPoint> GetPastData(String dataSourceName, 
             Variable variable, String coordinates, 
             LocalDate startDate, LocalDate endDate) {
@@ -45,6 +81,15 @@ public class ModelMain {
         return dataSources.get(dataSourceName).GetData(variable, coordinates, startDate, endDate);
     }
     
+    /**
+     * Returns forecast data of variable between two dates in given area
+     * @param dataSourceName Name of the dataSource to be queried
+     * @param variable The variable of which data is queried
+     * @param coordinates the coordinates over to be covered by the data
+     * @param startDate start date of the query
+     * @param endDate end date of the query
+     * @return List of dataPoints representing the queried data
+     */
     public List<DataPoint> GetForecastData(String dataSourceName, 
             Variable variable, String coordinates, 
             LocalDateTime startDate, LocalDateTime endDate) {
